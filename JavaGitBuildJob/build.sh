6ba7d75e1b2283f5eac0ee4e6132a8f91ea0acea
gowthamselvarajgit <gowtham26.work@gmail.com>
2025-06-20 12:26:26 +0530

    Job to build Java

--- JavaGitBuildJob/build.sh
+++ JavaGitBuildJob/build.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 echo "Starting Java Build Process"
 
 mkdir -p out
 
-javac -d out src/*.java
+javac -d out JavaGitBuildJob/src/*.java
 
 if [ $? -eq 0 ]; then
     echo "Java compilation successful"
 else
-    echo"Compilation Failed"
+    echo "Compilation Failed"
     exit 1
 fi
