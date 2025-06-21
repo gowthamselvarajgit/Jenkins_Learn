9751cf1bf02be01d48d458d7a61bcb0d8dcd8aa5
gowthamselvarajgit <gowtham26.work@gmail.com>
2025-06-21 16:30:47 +0530

    Docker with Terraform

--- TerraformDockerProject/terraform/variables.tf
+++ TerraformDockerProject/terraform/variables.tf
@@ -1,16 +1,16 @@
 variable "ami" {
-  
+  default = "ami-0f918f7e67a3323f0"
 }
 
 variable "instance_type" {
-  
+  default = "t2.micro"
 }
 
 variable "key_name" {
-  
+  default = "ec2"
 }
 
 variable "subnet_id" {
-  
+  default = "subnet-012030dcd933fb753"
 }
 
