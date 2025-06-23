f4352342bc2cad4067080aecb031c3977ffafea3
gowthamselvarajgit <gowtham26.work@gmail.com>
2025-06-23 11:04:43 +0530

    Learning Jenkins

--- DynamicEC2Docker/terraform/main.tf
+++ DynamicEC2Docker/terraform/main.tf
@@ -3,11 +3,27 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
+resource "aws_security_group" "sg" {
+  name = "sg"
+  vpc_id = "vpc-094c7dc3b90c938c6"
 
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 resource "aws_instance" "ec2" {
   ami = var.ami
   instance_type = var.instance_type
   key_name = var.key_name
   subnet_id = var.subnet_id
-  security_groups = [var.vpc_security_group_ids]
+  vpc_security_group_ids= [aws_security_group.sg.id]
 }
