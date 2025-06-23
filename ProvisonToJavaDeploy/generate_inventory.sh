d55c9b4403446dbcf316147672a6c98c517b0953
gowthamselvarajgit <gowtham26.work@gmail.com>
2025-06-23 14:51:53 +0530

    Jenkins Learn

--- ProvisonToJavaDeploy/generate_inventory.sh
+++ ProvisonToJavaDeploy/generate_inventory.sh
@@ -3,9 +3,7 @@
 cd terraform || exit 
 EC2_IP=$(terraform output -raw instance_ip)
 
-cat <<EOF > ../ansible/inventory.ini 
+cat <<EOF > ../ansible/inventory.ini
 [ec2]
 $EC2_IP ansible_user=ubuntu ansible_ssh_private_key_file=/home/gowtham/Downloads/key_pair/ec2.pem
-EOF 
-
-echo "Inventory created with IP: $EC2_IP"
+EOF
