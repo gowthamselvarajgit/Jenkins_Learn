812b8497c77c298589b7ba1aee92ea1b918e6c45
gowthamselvarajgit <gowtham26.work@gmail.com>
2025-06-23 14:32:11 +0530

    Jenkins Learn

--- ProvisonToJavaDeploy/terraform/main.tf
+++ ProvisonToJavaDeploy/terraform/main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "ec2" {
   key_name = var.key_name
   subnet_id = var.subnet_id
   vpc_security_group_ids = [aws_security_group.sg.id]
-
+  associate_public_ip_address = true
   tags = {
     Name = "java-app-instance"
   }
