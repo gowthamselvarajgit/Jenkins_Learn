9c3f897102200589ec9434a8cd397c73744e598e
gowthamselvarajgit <gowtham26.work@gmail.com>
2025-06-23 11:10:41 +0530

    Learning Jenkins

--- DynamicEC2Docker/terraform/main.tf
+++ DynamicEC2Docker/terraform/main.tf
@@ -26,4 +26,5 @@ resource "aws_instance" "ec2" {
   key_name = var.key_name
   subnet_id = var.subnet_id
   vpc_security_group_ids= [aws_security_group.sg.id]
+  associate_public_ip_address = true
 }
