c0336fabdbe25f97637985a452fc0fed1be12507
gowthamselvarajgit <gowtham26.work@gmail.com>
2025-06-23 15:48:58 +0530

    Learning Jenkins

--- ProvisonToJavaDeploy/terraform/main.tf
+++ ProvisonToJavaDeploy/terraform/main.tf
@@ -4,6 +4,42 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = ["10.0.0.0/16"]
+  tags = {
+    Name = "Main-VPC"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+
+resource "aws_subnet" "subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "ap-south-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "publicSubnet"
+  }
+}
+
+resource "aws_route_table_association" "rt_assoc" {
+  subnet_id = aws_subnet.subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
 resource "aws_security_group" "sg" {
   name = "sg"
   vpc_id = var.vpc_id
