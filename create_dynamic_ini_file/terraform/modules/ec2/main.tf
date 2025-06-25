9eb61750de535479ced98a7425365e6f613082ee
gowthamselvarajgit <gowtham26.work@gmail.com>
2025-06-25 11:24:43 +0530

    Learning Jenkins

--- create_dynamic_ini_file/terraform/modules/ec2/main.tf
+++ create_dynamic_ini_file/terraform/modules/ec2/main.tf
@@ -39,6 +39,7 @@ resource "aws_instance" "ec2MyInstance" {
   key_name = var.key_name
   subnet_id = aws_subnet.subnet.id
   vpc_security_group_ids = [aws_security_group.sg.id]
+  associate_public_ip_address = true
   tags = {
     Name = var.name
   }
